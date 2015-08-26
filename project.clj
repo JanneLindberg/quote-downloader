f7909b17ead623e18c4d12d814885b8beba3ce97
JanneLindberg <janne-lindberg@bahnhof.se>
2015-08-26 06:16:29 +0200

    Build with travis

--- project.clj
+++ project.clj
@@ -6,7 +6,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [
-                 [org.clojure/clojure "1.6.0"]
                  [http-kit "2.1.18"]
                  [clj-time "0.11.0"]
-                ])
+                 ]
+
+  :profiles {
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             }
+  )
